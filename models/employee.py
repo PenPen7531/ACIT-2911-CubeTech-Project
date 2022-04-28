7485c49e4f640d79d1352a3a73309fc5e21a972f
Hareem Fatima <hfatima@my.bcit.ca>
2022-04-28 12:54:59 -0700

    Created the employee class

---
+++ models/employee.py
@@ -0,0 +1,5 @@
+class Employee:
+    def __init__(self, first_name, last_name, employee_id, employee_department, employee_salary, employee_age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.employee_id = employee_id
