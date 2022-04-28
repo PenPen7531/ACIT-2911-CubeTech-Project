02fb05199bf3289db1b0e0050c0ea9b731583cc1
Hareem Fatima <hfatima@my.bcit.ca>
2022-04-28 15:00:57 -0700

    Made changes to the employee class

--- models/employee.py
+++ models/employee.py
@@ -27,6 +27,7 @@ class Employee:
     def to_dict(self):
         '''
         this methods creates an array of employees
+        and returns the dict of all the employees
         '''
         return {
             "first_name": self.first_name,
