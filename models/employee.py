c530c6f22b1e9892bdf092634dd4e9a96aa5d28a
Jeff7531 <jeffmkwang@gmail.com>
2022-04-28 14:25:57 -0700

    Added Type checks for employee __init__ function

--- models/employee.py
+++ models/employee.py
@@ -1,10 +1,26 @@
 class Employee:
     def __init__(self, first_name, last_name, employee_id, employee_department, employee_salary, employee_age):
+        if type(first_name) != str:
+            raise TypeError
         self.first_name = first_name
+        if type(last_name) != str:
+            raise TypeError
         self.last_name = last_name
+
+        if type(employee_id)!=str:
+            raise TypeError
         self.employee_id = employee_id
+
+        if type(employee_department) != str:
+            raise TypeError
         self.employee_department = employee_department
+
+        if type(employee_salary)!=int:
+            raise TypeError
         self.employee_salary = employee_salary
+
+        if type(employee_age)!= int:
+            raise TypeError
         self.employee_age = employee_age
 
 # add typeerrors for the classes
