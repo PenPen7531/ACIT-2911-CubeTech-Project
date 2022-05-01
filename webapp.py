4aeb288213f1f76c5f609f4ce0d108e3b31fdb0e
Hareem Fatima <hfatima@my.bcit.ca>
2022-05-01 01:35:56 -0700

    Added a post method to flask interface

--- webapp.py
+++ webapp.py
@@ -26,6 +26,18 @@ def homepage():
 def create_page():
     if request.method == "GET":
         return render_template("create.html")
+    if request.method == "POST":
+        employee_fname = request.form.get("firstname")
+        employee_lname = request.form.get("last_name")
+        employee_id = request.form.get("employee_id")
+        employee_department = request.form.get("employee_department")
+        employee_salary = request.form.get("employee_salary")
+        employee_age = request.form.get("employee_age")
+        new_emp = Employee(employee_fname, employee_lname, employee_id,
+                           employee_department, employee_salary, employee_age)
+        COMPANY.add(new_emp)
+        COMPANY.save()
+        return render_template("home.html", company=COMPANY)
 
 
 if __name__ == "__main__":
