8fa02046354412848aac9bb6515fe049419fc2c1
brandon_w laptop <bwoo14@my.bcit.ca>
2022-05-03 12:16:27 -0700

    Add unit tests for init function in the admin class

--- models/test_admin.py
+++ models/test_admin.py
@@ -1,4 +1,26 @@
+
+# Command used to test coverage of pytest 
+# pytest --cov
+
 import pytest
+import admin
 
 
+def test__init__success():
+    admin_test = admin.Admin('admin', 'root', 'root_database')
+    
+    assert admin_test.username == 'admin'
+    assert admin_test.password == 'root'
+    assert admin_test.database == 'root_database'
+
+
+def test_init_failure():
+    with pytest.raises(TypeError):
+        admin.Admin(5, 'root', 'root_database')
+    
+    with pytest.raises(TypeError):
+        admin.Admin('admin', 5, 'root_database')
+
+    with pytest.raises(TypeError):
+        admin.Admin('admin', 'root', 5)
 
